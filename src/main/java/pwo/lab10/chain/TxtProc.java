9aac3b2bfe250e52244b940fe4c08db01361ecab
Vladyslav <vladyslav.hutsenko@pollub.edu.pl>
2022-11-21 12:37:55 +0100

    Dodanie plikow

--- src/main/java/pwo/lab10/chain/TxtProc.java
+++ src/main/java/pwo/lab10/chain/TxtProc.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pwo.lab10.chain;
 
-/**
- *
- * @author pidoras
- */
-public class TxtProc {
-    
+public abstract class TxtProc implements
+        Request.RequestHandler {
+
+    protected TxtProc next = null;
+
+    public void add(TxtProc txtProc) {
+        if (next == null) {
+            next = txtProc;
+        } else {
+            next.add(txtProc);
+        }
+    }
+
+    protected String next(Request request) {
+        if (next != null) {
+            return next.processRequest(request);
+        } else {
+            return null;
+        }
+    }
 }
